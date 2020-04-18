e7813749b568ba47cfe3b4a0b056f0a44065d6d0
Karolin P <porrask92@gmail.com>
2020-04-18 18:51:39 -0500

    nav component / constructor project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import NavMenu from './components/Nav'
+function App() {
+  return (
+    <div className="App">
+      <NavMenu />
+    </div>
+  );
+}
+
+export default App;
