af078f66efaca54d6ec3ee7586b2df85bcc320ad
Karolin P <porrask92@gmail.com>
2020-04-19 18:53:05 -0500

    top bar color

--- src/App.js
+++ src/App.js
@@ -2,7 +2,16 @@ import React from 'react';
 import './App.css';
 import NavMenu from './components/Nav'
 import Dashboard from './components/Dashboard'
+import TopBarProgress from "react-topbar-progress-indicator";
 
+
+TopBarProgress.config({
+  barColors: {
+    "0": "#2bab38",
+    "1.0": "#fff"
+  },
+  shadowBlur: 5
+});
 
 function App() {
   return (
